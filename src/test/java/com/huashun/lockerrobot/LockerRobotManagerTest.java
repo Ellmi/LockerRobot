b4349acd137ab78f4b97756e2c5a4fd88b4dea0e
Yanmin XING <yanmin@Yanmins-MacBook-Pro.local>
2020-09-21 10:17:47 +0800

    refactor to atract a method to build manager

--- src/test/java/com/huashun/lockerrobot/LockerRobotManagerTest.java
+++ src/test/java/com/huashun/lockerrobot/LockerRobotManagerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static com.huashun.lockerrobot.SizeType.L;
+import static com.huashun.lockerrobot.SizeType.M;
 import static com.huashun.lockerrobot.SizeType.S;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -14,19 +15,22 @@ public class LockerRobotManagerTest {
 
     @Test
     public void should_return_s_locker_type_ticket_when_store_small_bag_given_robot_manage_1_free_s_locker_and_1_primaryLockerRobot_and_1_superLockerRobot() {
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(List.of(new Locker(S, 1)));
+        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
 
         Ticket ticket = lockerRobotManager.store(new Bag(S));
 
         assertEquals(S, ticket.getLockerSizeType());
     }
 
+    private LockerRobotManager buildLockerRobotManager() {
+        return new LockerRobotManager(List.of(new Locker(S, 1), new PrimaryLockerRobot(List.of(new Locker(M, 1))), new SuperLockerRobot(List.of(new Locker(L, 1)))));
+    }
+
     @Test
     public void should_throw_LockerIsFullException_when_store_small_bag_given_robot_manage_1_full_s_locker_and_1_primaryLockerRobot_and_1_superLockerRobot() {
-        Locker locker = new Locker(S, 1);
-        locker.store(new Bag(S));
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(List.of(locker));
 
+        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
+        lockerRobotManager.store(new Bag(S));
 
         assertThrows(LockerIsFullException.class, () -> lockerRobotManager.store(new Bag(S)));
     }
