3bd1b5af57579f52777e916ffe8cb11e99d7dac2
Yanmin XING <yanmin@Yanmins-MacBook-Pro.local>
2020-09-21 10:13:24 +0800

    manger should throw locker is full exception when store s bag given full manged s locker

--- src/test/java/com/huashun/lockerrobot/LockerRobotManagerTest.java
+++ src/test/java/com/huashun/lockerrobot/LockerRobotManagerTest.java
@@ -1,5 +1,6 @@
 package com.huashun.lockerrobot;
 
+import com.huashun.lockerrobot.exception.LockerIsFullException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 import static com.huashun.lockerrobot.SizeType.L;
 import static com.huashun.lockerrobot.SizeType.S;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LockerRobotManagerTest {
 
@@ -17,6 +19,16 @@ public class LockerRobotManagerTest {
         Ticket ticket = lockerRobotManager.store(new Bag(S));
 
         assertEquals(S, ticket.getLockerSizeType());
+    }
+
+    @Test
+    public void should_throw_LockerIsFullException_when_store_small_bag_given_robot_manage_1_full_s_locker_and_1_primaryLockerRobot_and_1_superLockerRobot() {
+        Locker locker = new Locker(S, 1);
+        locker.store(new Bag(S));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(List.of(locker));
+
+
+        assertThrows(LockerIsFullException.class, () -> lockerRobotManager.store(new Bag(S)));
     }
 
 }
