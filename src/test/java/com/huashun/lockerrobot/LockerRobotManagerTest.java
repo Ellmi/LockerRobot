1da9cb02752b494d8b7fd8d9dc03d1ace88f9974
Yanmin XING <yanmin@Yanmins-MacBook-Pro.local>
2020-09-21 10:40:55 +0800

    refactor to use parameterized to reduce test code

--- src/test/java/com/huashun/lockerrobot/LockerRobotManagerTest.java
+++ src/test/java/com/huashun/lockerrobot/LockerRobotManagerTest.java
@@ -3,6 +3,8 @@ package com.huashun.lockerrobot;
 import com.huashun.lockerrobot.exception.InvalidTicketException;
 import com.huashun.lockerrobot.exception.LockerIsFullException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.List;
 
@@ -15,68 +17,35 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class LockerRobotManagerTest {
 
-    @Test
-    public void should_return_s_locker_type_ticket_when_store_small_bag_given_robot_manage_1_free_s_locker_and_1_primaryLockerRobot_and_1_superLockerRobot() {
+    @ParameterizedTest
+    @EnumSource(SizeType.class)
+    public void should_return_correct_locker_type_ticket_when_store_different_size_bag_given_robot_manage_1_s_locker_and_1_primaryLockerRobot_and_1_superLockerRobot_which_all_free(SizeType sizeType) {
         LockerRobotManager lockerRobotManager = buildLockerRobotManager();
 
-        Ticket ticket = lockerRobotManager.store(new Bag(S));
+        Ticket ticket = lockerRobotManager.store(new Bag(sizeType));
 
-        assertEquals(S, ticket.getLockerSizeType());
+        assertEquals(sizeType, ticket.getLockerSizeType());
     }
 
     private LockerRobotManager buildLockerRobotManager() {
         return new LockerRobotManager(List.of(new Locker(S, 1), new PrimaryLockerRobot(List.of(new Locker(M, 1))), new SuperLockerRobot(List.of(new Locker(L, 1)))));
     }
 
-    @Test
-    public void should_throw_LockerIsFullException_when_store_small_bag_given_robot_manage_1_full_s_locker_and_1_primaryLockerRobot_and_1_superLockerRobot() {
+    @ParameterizedTest
+    @EnumSource(SizeType.class)
+    public void should_throw_LockerIsFullException_when_store_different_size_bag_given_robot_manage_1_s_locker_and_1_primaryLockerRobot_and_1_superLockerRobot_which_all_full(SizeType sizeType) {
 
         LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        lockerRobotManager.store(new Bag(S));
+        lockerRobotManager.store(new Bag(sizeType));
 
-        assertThrows(LockerIsFullException.class, () -> lockerRobotManager.store(new Bag(S)));
+        assertThrows(LockerIsFullException.class, () -> lockerRobotManager.store(new Bag(sizeType)));
     }
 
-    @Test
-    public void should_return_m_locker_type_ticket_when_store_medium_bag_given_robot_manage_1_s_locker_and_1_free_primaryLockerRobot_and_1_superLockerRobot() {
+    @ParameterizedTest
+    @EnumSource(SizeType.class)
+    public void should_return_correct_bag_when_fetch_bag_given_valid_different_locker_size_ticket(SizeType sizeType) {
         LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-
-        Ticket ticket = lockerRobotManager.store(new Bag(M));
-
-        assertEquals(M, ticket.getLockerSizeType());
-    }
-
-    @Test
-    public void should_throw_LockerIsFullException_when_store_medium_bag_given_robot_manage_1_s_locker_and_1_full_primaryLockerRobot_and_1_superLockerRobot() {
-
-        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        lockerRobotManager.store(new Bag(M));
-
-        assertThrows(LockerIsFullException.class, () -> lockerRobotManager.store(new Bag(M)));
-    }
-
-    @Test
-    public void should_return_l_locker_type_ticket_when_store_large_bag_given_robot_manage_1_s_locker_and_1_primaryLockerRobot_and_1_free_superLockerRobot() {
-        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-
-        Ticket ticket = lockerRobotManager.store(new Bag(L));
-
-        assertEquals(L, ticket.getLockerSizeType());
-    }
-
-    @Test
-    public void should_throw_LockerIsFullException_when_store_large_bag_given_robot_manage_1_s_locker_and_1primaryLockerRobot_and_1_full_superLockerRobot() {
-
-        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        lockerRobotManager.store(new Bag(L));
-
-        assertThrows(LockerIsFullException.class, () -> lockerRobotManager.store(new Bag(L)));
-    }
-
-    @Test
-    public void should_return_correct_bag_when_fetch_bag_given_valid_s_ticket() {
-        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        Bag storedBag = new Bag(S);
+        Bag storedBag = new Bag(sizeType);
         Ticket ticket = lockerRobotManager.store(storedBag);
 
         Bag fetchedBag = lockerRobotManager.fetchBagBy(ticket);
@@ -84,52 +53,14 @@ public class LockerRobotManagerTest {
         assertSame(storedBag, fetchedBag);
     }
 
-    @Test
-    public void should_throw_InvalidTicketException_when_fetch_bag_given_invalid_s_ticket() {
+    @ParameterizedTest
+    @EnumSource(SizeType.class)
+    public void should_throw_InvalidTicketException_when_fetch_bag_given_invalid_different_locker_size_ticket(SizeType sizeType) {
         LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        Bag storedBag = new Bag(S);
+        Bag storedBag = new Bag(sizeType);
         lockerRobotManager.store(storedBag);
 
-        assertThrows(InvalidTicketException.class, () -> lockerRobotManager.fetchBagBy(new Ticket(S)));
+        assertThrows(InvalidTicketException.class, () -> lockerRobotManager.fetchBagBy(new Ticket(sizeType)));
     }
 
-    @Test
-    public void should_return_correct_bag_when_fetch_bag_given_valid_m_ticket() {
-        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        Bag storedBag = new Bag(M);
-        Ticket ticket = lockerRobotManager.store(storedBag);
-
-        Bag fetchedBag = lockerRobotManager.fetchBagBy(ticket);
-
-        assertSame(storedBag, fetchedBag);
-    }
-
-    @Test
-    public void should_throw_InvalidTicketException_when_fetch_bag_given_invalid_m_ticket() {
-        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        Bag storedBag = new Bag(M);
-        lockerRobotManager.store(storedBag);
-
-        assertThrows(InvalidTicketException.class, () -> lockerRobotManager.fetchBagBy(new Ticket(M)));
-    }
-
-    @Test
-    public void should_return_correct_bag_when_fetch_bag_given_valid_l_ticket() {
-        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        Bag storedBag = new Bag(L);
-        Ticket ticket = lockerRobotManager.store(storedBag);
-
-        Bag fetchedBag = lockerRobotManager.fetchBagBy(ticket);
-
-        assertSame(storedBag, fetchedBag);
-    }
-
-    @Test
-    public void should_throw_InvalidTicketException_when_fetch_bag_given_invalid_l_ticket() {
-        LockerRobotManager lockerRobotManager = buildLockerRobotManager();
-        Bag storedBag = new Bag(L);
-        lockerRobotManager.store(storedBag);
-
-        assertThrows(InvalidTicketException.class, () -> lockerRobotManager.fetchBagBy(new Ticket(L)));
-    }
 }
